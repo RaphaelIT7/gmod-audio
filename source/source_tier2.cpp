47a8d17ea23a6ca8658db7dd48475480ccdc4a9f
RaphaelIT7 <raphael.i.t@outlook.de>
2024-05-21 07:16:32 +0200

    Strip it :<

--- source/source_tier2.cpp
+++ source/source_tier2.cpp
@@ -7,17 +7,6 @@
 #include <tier2/tier2.h>
 #include "tier0/dbg.h"
 #include "filesystem.h"
-#include "materialsystem/imaterialsystem.h"
-#include "materialsystem/imaterialsystemhardwareconfig.h"
-#include "materialsystem/IColorCorrection.h"
-#include "materialsystem/idebugtextureinfo.h"
-#include "materialsystem/ivballoctracker.h"
-#include "inputsystem/iinputsystem.h"
-#include "networksystem/inetworksystem.h"
-// dimhotepus: No perforce
-// #include "p4lib/ip4.h"
-#include "mdllib/mdllib.h"
-#include "filesystem/IQueuedLoader.h"
 
 
 //-----------------------------------------------------------------------------
@@ -27,18 +16,6 @@
 // allowing link libraries to access tier2 library interfaces
 //-----------------------------------------------------------------------------
 IFileSystem *g_pFullFileSystem = 0;
-IMaterialSystem *materials = 0;
-IMaterialSystem *g_pMaterialSystem = 0;
-IInputSystem *g_pInputSystem = 0;
-INetworkSystem *g_pNetworkSystem = 0;
-IMaterialSystemHardwareConfig *g_pMaterialSystemHardwareConfig = 0;
-IDebugTextureInfo *g_pMaterialSystemDebugTextureInfo = 0;
-IVBAllocTracker *g_VBAllocTracker = 0;
-IColorCorrectionSystem *colorcorrection = 0;
-IP4 *p4 = 0;
-IMdlLib *mdllib = 0;
-IQueuedLoader *g_pQueuedLoader = 0;
-
 
 //-----------------------------------------------------------------------------
 // Call this to connect to all tier 2 libraries.
@@ -47,9 +24,7 @@ IQueuedLoader *g_pQueuedLoader = 0;
 void ConnectTier2Libraries( CreateInterfaceFn *pFactoryList, int nFactoryCount )
 {
 	// Don't connect twice..
-	Assert( !g_pFullFileSystem && !materials && !g_pInputSystem && !g_pNetworkSystem && 
-		!p4 && !mdllib && !g_pMaterialSystemDebugTextureInfo && !g_VBAllocTracker &&
-		!g_pMaterialSystemHardwareConfig && !g_pQueuedLoader );
+	Assert( !g_pFullFileSystem );
 
 	for ( int i = 0; i < nFactoryCount; ++i )
 	{
@@ -57,62 +32,11 @@ void ConnectTier2Libraries( CreateInterfaceFn *pFactoryList, int nFactoryCount )
 		{
 			g_pFullFileSystem = ( IFileSystem * )pFactoryList[i]( FILESYSTEM_INTERFACE_VERSION, NULL );
 		}
-		if ( !materials )
-		{
-			g_pMaterialSystem = materials = ( IMaterialSystem * )pFactoryList[i]( MATERIAL_SYSTEM_INTERFACE_VERSION, NULL );
-		}
-		if ( !g_pInputSystem )
-		{
-			g_pInputSystem = ( IInputSystem * )pFactoryList[i]( INPUTSYSTEM_INTERFACE_VERSION, NULL );
-		}
-		if ( !g_pNetworkSystem )
-		{
-			g_pNetworkSystem = ( INetworkSystem * )pFactoryList[i]( NETWORKSYSTEM_INTERFACE_VERSION, NULL );
-		}
-		if ( !g_pMaterialSystemHardwareConfig )
-		{
-			g_pMaterialSystemHardwareConfig = ( IMaterialSystemHardwareConfig * )pFactoryList[i]( MATERIALSYSTEM_HARDWARECONFIG_INTERFACE_VERSION, NULL );
-		}
-		if ( !g_pMaterialSystemDebugTextureInfo )
-		{
-			g_pMaterialSystemDebugTextureInfo = (IDebugTextureInfo*)pFactoryList[i]( DEBUG_TEXTURE_INFO_VERSION, 0 );
-		}
-		if ( !g_VBAllocTracker )
-		{
-			g_VBAllocTracker = (IVBAllocTracker*)pFactoryList[i]( VB_ALLOC_TRACKER_INTERFACE_VERSION, 0 );
-		}
-		// dimhotepus: No perforce
-		/*if ( !p4 )
-		{
-			p4 = ( IP4 * )pFactoryList[i]( P4_INTERFACE_VERSION, NULL );
-		}*/
-		if ( !colorcorrection )
-		{
-			colorcorrection = ( IColorCorrectionSystem * )pFactoryList[i]( COLORCORRECTION_INTERFACE_VERSION, NULL );
-		}
-		if ( !mdllib )
-		{
-			mdllib = ( IMdlLib * )pFactoryList[i]( MDLLIB_INTERFACE_VERSION, NULL );
-		}
-		if ( !g_pQueuedLoader )
-		{
-			g_pQueuedLoader = (IQueuedLoader *)pFactoryList[i]( QUEUEDLOADER_INTERFACE_VERSION, NULL );
-		}
 	}
 }
 
 void DisconnectTier2Libraries()
 {
-
 	g_pFullFileSystem = 0;
-	materials = g_pMaterialSystem = 0;
-	g_pMaterialSystemHardwareConfig = 0;
-	g_pMaterialSystemDebugTextureInfo = 0;
-	g_pInputSystem = 0;
-	g_pNetworkSystem = 0;
-	colorcorrection = 0;
-	p4 = 0;
-	mdllib = 0;
-	g_pQueuedLoader = 0;
 }
 
